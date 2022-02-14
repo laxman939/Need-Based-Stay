abe62a9e56ef7e2baf4aa3f8ad1b2d6aed47bf26
laxman <ramlaxman947@gmail.com>
2022-02-14 21:17:19 +0530

    Room type filter

--- src/Components/Home/Filters/RoomFilter.js
+++ src/Components/Home/Filters/RoomFilter.js
@@ -8,22 +8,22 @@ export default function RoomFilter() {
       <FormLabel id="roomtype">Room Type: </FormLabel>
       <div className="form-box">
         <label htmlFor="single">
-          <input type="radio" name="room" id="single" />
+          <input type="checkbox" name="room" id="single" />
           <div className="circle"></div>
           <span className="filter-type-name">Single Share</span>
         </label>
         <label htmlFor="two">
-          <input type="radio" name="room" id="two" />
+          <input type="checkbox" name="room" id="two" />
           <div className="circle"></div>
           <span className="filter-type-name">Two Share</span>
         </label>
         <label htmlFor="three">
-          <input type="radio" name="room" id="three" />
+          <input type="checkbox" name="room" id="three" />
           <div className="circle"></div>
           <span className="filter-type-name">Three Share</span>
         </label>
         <label htmlFor="four">
-          <input type="radio" name="room" id="four" />
+          <input type="checkbox" name="room" id="four" />
           <div className="circle"></div>
           <span className="filter-type-name">Four Share</span>
         </label>
