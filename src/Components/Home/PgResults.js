5a0e711dd60cf6bc2911ccf7f1501d4ed563873c
laxman <ramlaxman947@gmail.com>
2022-02-17 23:38:01 +0530

    Pgs Results

--- src/Components/Home/PgResults.js
+++ src/Components/Home/PgResults.js
@@ -4,38 +4,13 @@ function PgResults() {
   return (
     <div className="property-container">
       <div className="row single-property">
-        <div className="row">
+        <div className="row row-one">
           <div className="col">
             <h5>Pg1</h5>
             <p>Location</p>
           </div>
         </div>
-        <hr />
-        <div className="row">
-          <div className="col">
-            <img
-              id="image"
-              src="https://st4.depositphotos.com/16030310/25210/v/600/depositphotos_252105266-stock-illustration-vector-illustration-silver-golden-letters.jpg"
-              alt="house"
-            />
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-          <div className="col">
-            <h5>Details</h5>
-          </div>
-        </div>
-      </div>
-      <div className="row single-property">
-        <div className="row">
-          <div className="col">
-            <h5>Pg1</h5>
-            <p>Location</p>
-          </div>
-        </div>
-        <hr />
-        <div className="row">
+        <div className="row row-two">
           <div className="col">
             <img
               id="image"
