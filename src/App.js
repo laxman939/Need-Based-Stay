e4da38fbe3132f020cd1367da4ed17bf7d9624c3
laxman <ramlaxman947@gmail.com>
2022-02-18 23:15:36 +0530

    Restructure the code

--- src/App.js
+++ src/App.js
@@ -31,3 +31,10 @@ function App() {
 }
 
 export default App;
+/* <Route exact path="/signup">
+  {!isSignup ? (
+    <SignUp handleSignupUser={handleSignupUser} />
+  ) : (
+    <Redirect to="/signin" />
+  )}
+</Route>; */
