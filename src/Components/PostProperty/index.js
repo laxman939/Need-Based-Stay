97156e1e0f6e7f81bc1593baab845ba8e6a9b061
laxman <ramlaxman947@gmail.com>
2022-02-20 02:48:44 +0530

    Restructure the code

--- src/Components/PostProperty/index.js
+++ src/Components/PostProperty/index.js
@@ -1,38 +1,98 @@
 import React from "react";
-import Role from "../SignUp/Role";
+import StayType from "./StayType";
+import "./style.css";
 
 function PostProperty() {
   return (
     <>
-      <Role />
       <form className="post-property-container">
         <fieldset>
-          <legend>Property Details</legend>
-          <label for="ownerName">Owner Name</label>
-          <input type="text" id="ownerName" name="ownerName" />
+          <legend>Property Details:</legend>
+          <div>
+            <StayType />
+          </div>
 
-          <label for="pName">Property Name</label>
-          <input type="text" id="pName" name="pName" />
+          <div>
+            <label for="ownerName">Owner Name: &nbsp;</label>
+            <input type="text" id="ownerName" name="ownerName" />
 
-          <label for="phoneNumber">Phone Number</label>
-          <input type="text" id="phoneNumber" name="phoneNumber" />
+            <label for="pName">Property Name: &nbsp;</label>
+            <input type="text" id="pName" name="pName" />
+          </div>
 
-          <label for="pLocation">Property Location</label>
-          <input type="text" id="pLocation" name="pLocation" />
+          <div>
+            <label for="phoneNumber">Phone Number: &nbsp;</label>
+            <input type="text" id="phoneNumber" name="phoneNumber" />
 
-          <label for="pType">Property Type</label>
-          <input type="text" id="pType" name="pType" />
+            <label for="pLocation">Property Location: &nbsp;</label>
+            <input type="text" id="pLocation" name="pLocation" />
+          </div>
 
-          <label for="gender">Gender</label>
-          <input type="text" id="gender" name="gender" />
+          <div>
+            <label for="property-type">Location Type: &nbsp;</label>
+            <select id="property-type">
+              <option value="none">none</option>
+              <option value="city">City</option>
+              <option value="outskirts">Outskirts</option>
+            </select>
 
-          <label for="pLocation">Property Location</label>
-          <input type="text" id="pLocation" name="pLocation" />
+            <label for="gender">Gender: &nbsp;</label>
+            <select id="gender">
+              <option value=""></option>
+              <option value="both">Both</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+            </select>
+          </div>
 
-          <label for="pLocation">Property Location</label>
-          <input type="text" id="pLocation" name="pLocation" />
+          <div>
+            <label for="preference">Preference: &nbsp;</label>
+            <select id="preference">
+              <option value="both">Both</option>
+              <option value="Students">Students</option>
+              <option value="Professionals">Professionals</option>
+            </select>
 
-          <input type="submit" value="Submit" />
+            {/* Todo -- Check boxes or multiple options  */}
+            <label for="roomType">Room Type: &nbsp;</label>
+            <select id="roomType">
+              <option value="both">Both</option>
+              <option value="singleShare">Single Share</option>
+              <option value="twoShare">Two Share</option>
+              <option value="threeShare">Three Share</option>
+              <option value="fourShare">Four Share</option>
+            </select>
+          </div>
+
+          <div>{/*  priceRange-- Todo */}</div>
+
+          <div>
+            <label for="houseType">House Type: &nbsp;</label>
+            <select id="houseType">
+              <option value="flat">Flat</option>
+              <option value="independent">Independent</option>
+              <option value="gatedCommunity">Gated Community</option>
+            </select>
+
+            <label for="houseFor">House For: &nbsp;</label>
+            <select id="houseFor">
+              <option value="family">Family</option>
+              <option value="bachelors">Bachelor's</option>
+            </select>
+          </div>
+
+          <div>
+            {/*For NearBy places-- Todo*/}
+            <label for="nearBy">Near By: &nbsp;</label>
+            <input type="text" id="nearBy" name="nearBy" />
+          </div>
+
+          <div>
+            <label for="property-pics">Upload Images</label>
+            <input type="file" id="property-pics" />
+          </div>
+
+          <button type="submit">Submit</button>
         </fieldset>
       </form>
     </>
