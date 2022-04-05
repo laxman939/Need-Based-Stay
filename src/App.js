2e26ce9699a9ecf7dc904b154102c313f6a2bb71
laxman <ramlaxman947@gmail.com>
2022-04-05 17:07:41 +0530

    Modified Code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./style.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Components/Home";
 import NavigationBar from "./Components/Navbar";
 import OwnerPage from "./Components/OwnerPage";
@@ -11,19 +11,51 @@ import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp/Index";
 import UserPage from "./Components/UserPage";
 
+import { useSelector } from "react-redux";
+import Favourites from "./Components/Favourites/Favourites";
+
 function App() {
+  const { role } = useSelector((state) => state.Role);
+  const isSignup = useSelector((state) => state.Signup.signup);
+  const isSignin = useSelector((state) => state.Signin.signin);
+  console.log(
+    "APp.js " + role + "isSignup " + isSignup + "isSignin " + isSignin
+  );
+
   return (
     <div className="App">
       <BrowserRouter>
         <NavigationBar />
         <Routes>
           <Route path="/" exact element={<Home />} />
-          <Route path="/home" exact element={<Home />} />
-          <Route path="/postproperty" exact element={<PostProperty />} />
-          <Route path="/signup" exact element={<SignUp />} />
+          <Route path="/postproperty" element={<PostProperty />} />
+          <Route path="/favourites" element={<Favourites />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" exact element={<SignIn />} />
           <Route path="/owner" exact element={<OwnerPage />} />
-          <Route path="/user" exact element={<UserPage />} />
+          <Route path="/user" element={<UserPage />} />
+
+          {/* {role === "owner" && (
+            <Route path="/postproperty" element={<PostProperty />} />
+          )}
+
+          {role === "user" && (
+            <Route path="/favourites" element={<Favourites />} />
+          )}
+
+          <Route path="/signup" element={<SignUp />} />
+
+          {isSignup === true && (
+            <Route path="/signin" exact element={<SignIn />} />
+          )}
+
+          {role === "owner" ? (
+            <Route path="/owner" exact element={<OwnerPage />} />
+          ) : role === "user" ? (
+            <Route path="/user" element={<UserPage />} />
+          ) : (
+            <Route path="/signup" element={<SignUp />} />
+          )} */}
         </Routes>
       </BrowserRouter>
     </div>
