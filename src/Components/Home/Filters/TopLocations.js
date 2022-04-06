1ffaf1917f08ca0bb3ace310e393c0fd0f3dd9b3
laxman <ramlaxman947@gmail.com>
2022-04-06 12:04:01 +0530

    Modified Code

--- src/Components/Home/Filters/TopLocations.js
+++ src/Components/Home/Filters/TopLocations.js
@@ -1,53 +1,41 @@
 import * as React from "react";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 
-// const options = ["Option 1", "Option 2"];
-const names = [
-  "",
-  "Dilsukh Nagar",
-  "Mehidipatnam",
-  "Koti",
-  "Abids",
-  "Ibrahimpatnam",
-  "gacchibowli",
-  "LB nagar",
-  "Hasthinapuram",
-  "Uppal",
-  "Secundrabad",
-];
+import { Form, Select } from "antd";
 
 export default function TopLocations() {
-  const [value, setValue] = React.useState(names[0]);
-  const [inputValue, setInputValue] = React.useState("");
-
-  const [location, setLocation] = React.useState("");
-
-  // console.log("location " + location);
+  const { Option } = Select;
 
   return (
-    <div id="toplocation">
-      <Autocomplete
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={names}
-        renderInput={(params) => (
-          <TextField
-            className="top-location-textfield"
-            variant="standard"
-            {...params}
-            label="Select a Location"
-            onSelect={(e) => setLocation(e.target.value)}
-          />
-        )}
-      />
+    <div className="Top-section">
+      <Form.Item
+        id="top-formItem"
+        name="select-multiple"
+        label="Select Loaction: "
+        rules={[
+          {
+            message: `Top location`,
+            type: "array",
+          },
+        ]}
+      >
+        <Select
+          id="top-Select"
+          placeholder="Top Location 🔻"
+          onChange={(e) => console.log("Top Location " + e)}
+        >
+          <Option value="">None</Option>
+          <Option value="DilsukhNagar">Dilsukh Nagar</Option>
+          <Option value="Mehidipatnam">Mehidipatnam</Option>
+          <Option value="Koti">Koti</Option>
+          <Option value="Abids">Abids</Option>
+          <Option value="Ibrahimpatnam">Ibrahimpatnam</Option>
+          <Option value="gacchibowli">gacchibowli</Option>
+          <Option value="LBNagar">LB nagar</Option>
+          <Option value="Secundrabad">Secundrabad</Option>
+          <Option value="Uppal">Uppal</Option>
+          <Option value="Hasthinapuram">Hasthinapuram</Option>
+        </Select>
+      </Form.Item>
     </div>
   );
 }
